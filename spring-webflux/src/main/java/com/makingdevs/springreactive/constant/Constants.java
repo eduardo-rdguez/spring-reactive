bb66bb37bab616da904aab54999ad52fff298143
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-11-27 09:36:19 -0600

    Add constants class

---
+++ spring-webflux/src/main/java/com/makingdevs/springreactive/constant/Constants.java
@@ -0,0 +1,12 @@
+package com.makingdevs.springreactive.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Constants {
+
+  /* Numbers */
+  public static final int ONE_HUNDRED = 100;
+
+}
